d7608af5ec8309d452c0b9f8349488d80c5b240b
Amina <aminchik2809@gmail.com>
2024-05-02 16:51:55 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Homework3._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя: ");
+            var name = Console.ReadLine();
+            Console.Write("Введите возраст: ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите дату рождения: ");
+            var birthdate = Console.ReadLine();
+        }
+    }
+}
